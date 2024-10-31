ef08ac0c05e3d57231f35204cc99a7993c51b17a
Chomusuke <yukhanov.artur@gmail.com>
2024-10-31 15:39:51 +0100

    Adjusted constructor

--- sources/Group.java
+++ sources/Group.java
@@ -1,16 +1,25 @@
+import people.Etudiant;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Group {
 	private final int number;
 	private final String orientation;
 	private final int quarter;
-	private final List<Object> students = new ArrayList<>();  // TODO list of students
+	private final List<Etudiant> students = new ArrayList<>();
+	private final List<Object> lessons = new ArrayList<>();
 
-	public Group(int number, String orientation, int quarter) {
+	public Group(int number, String orientation, int quarter, Etudiant ... students) {
+		if (students.length < 1) {
+			throw new IllegalArgumentException("A group must have at least one student");
+		}
+
 		this.number = number;
 		this.orientation = orientation;
 		this.quarter = quarter;
+		this.students.addAll(Arrays.asList(students));
 	}
 
 	public String horaire() {
